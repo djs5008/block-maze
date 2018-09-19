03669215f03988584107e1673dda7e5da2a1628c
Dan Schmitt <djs5008@rit.edu>
2018-09-19 01:03:38 -0400

    direction of moves are now mapped and shown nicely

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -37,28 +37,122 @@ class Tile extends Component {
     return result;
   }
 
+  /**
+   * Retrieve the index of the move object related to this tile
+   *  Can be used for indexing forward/backward moves
+   */
+  getMoveIndex() {
+    let i = -1;
+    let x = this.props.col;
+    let y = this.props.row;
+    this.props.moves.forEach((move, index) => {
+      if (move.x === x && move.y === y) {
+        i = index;
+        return;
+      }
+    });
+    return i;
+  }
+
+  /**
+   * Retrieve the CSS class for the "coming from" connector direction
+   */
+  getFirstConnDir() {
+    let index = this.getMoveIndex();
+    let x = this.props.col;
+    let y = this.props.row;
+
+    // Make sure this isn't the first-made move
+    if (index !== -1) {
+      if (this.props.moves.length > 1) {
+        // Check direction of previous move
+        let prevMove = this.props.moves[index-1];
+        if (prevMove !== undefined) {
+          if (prevMove.x < x) {
+            return 'tile-direction-left';
+          } else if (prevMove.x > x) {
+            return 'tile-direction-right';
+          } else if (prevMove.y < y) {
+            return 'tile-direction-up';
+          } else if (prevMove.y > y) {
+            return 'tile-direction-down';
+          }
+        }
+      }
+    }
+    return '';
+  }
+
+  /**
+   * Retreive the CSS clas for the "leading to" connector direction
+   */
+  getSecondConnDir() {
+    let index = this.getMoveIndex();
+    let x = this.props.col;
+    let y = this.props.row;
+
+    // Make sure this isn't the last-made move
+    if (index !== -1) {
+      if (this.props.moves.length > (index+1)) {
+        // Check direction of next move
+        let nextMove = this.props.moves[index+1];
+        if (nextMove.x < x) {
+          return 'tile-direction-left';
+        } else if (nextMove.x > x) {
+          return 'tile-direction-right';
+        } else if (nextMove.y < y) {
+          return 'tile-direction-up';
+        } else if (nextMove.y > y) {
+          return 'tile-direction-down';
+        }
+      }
+    }
+
+    return '';
+  }
+
   render() {
     // Render proper tile type
     switch (this.getType()) {
       case 'START':
         return (
-          <div className={`tile-root tile-start`} onMouseOver={this.props.onMouseOver}></div>
+          <div>
+            <div className={`${this.getFirstConnDir()}`}></div>
+            <div className={`${this.getSecondConnDir()}`}></div>
+            <div className={`tile-root tile-start`} onMouseOver={this.props.onMouseOver}></div>
+          </div>
         );
       case 'BLANK':
         return (
-          <div className={`tile-root tile-gap`}></div>
+          <div>
+            <div className={`${this.getFirstConnDir()}`}></div>
+            <div className={`${this.getSecondConnDir()}`}></div>
+            <div className={`tile-root tile-gap`}></div>  
+          </div>
         );
       case 'MOVE':
         return (
-          <div className={`tile-root tile-moved`} onMouseOver={this.props.onMouseOver}></div>
+          <div>
+            <div className={`${this.getFirstConnDir()}`}></div>
+            <div className={`${this.getSecondConnDir()}`}></div>
+            <div className={`tile-root tile-moved`} onMouseOver={this.props.onMouseOver}></div>
+          </div>
         );
       case 'WON':
         return (
-          <div className={`tile-root tile-won`} onMouseOver={this.props.onMouseOver}></div>
+          <div>
+            <div className={`${this.getFirstConnDir()}`}></div>
+            <div className={`${this.getSecondConnDir()}`}></div>
+            <div className={`tile-root tile-won`} onMouseOver={this.props.onMouseOver}></div>
+          </div>
         );
       default:
         return (
-          <div className={`tile-root tile-normal`} onMouseOver={this.props.onMouseOver}></div>
+          <div>
+            <div className={`${this.getFirstConnDir()}`}></div>
+            <div className={`${this.getSecondConnDir()}`}></div>
+            <div className={`tile-root tile-normal`} onMouseOver={this.props.onMouseOver}></div>
+          </div>
         );
     }
   }
